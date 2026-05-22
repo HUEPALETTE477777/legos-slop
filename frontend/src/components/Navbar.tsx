2efdc6cf076fac42d8780ce1176507473263047c
austin <ivanguan18@gmail.com>
2026-05-22 10:43:51 -0700

    asdasd

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,6 +1,8 @@
 import { Link } from 'react-router-dom'
 
 const Navbar = () => {
+    const slideLink = "https://docs.google.com/presentation/d/1chOtLuiiNbvTNIkDoQN-Nz79dVFQLFUUB8Qg4_lVJ5M/edit?usp=sharing";
+
     return (
         <nav className="sticky top-0 z-50 backdrop-blur-md bg-zinc-950/80 border-b border-zinc-800 px-6 py-4 flex items-center justify-evenly">
             <Link to="/" className="text-xl font-black tracking-tighter text-zinc-100 uppercase hover:text-red-500 transition-colors">
@@ -15,6 +17,9 @@ const Navbar = () => {
             <Link to="/swag" className="text-xl font-black uppercase hover:text-red-500 transition-colors">
                 Swag
             </Link>
+            <a href={slideLink} className="text-xl font-black uppercase hover:text-red-500 transition-colors">
+                Our Slides
+            </a>
         </nav>
     )
 }
